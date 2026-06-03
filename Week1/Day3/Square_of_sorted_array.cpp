02bd38dbcab05e64e19d1a3c6135ae78f2ffaa1e
parasseth2006 <parasseth2006@gmail.com>
2026-06-03 20:18:42 +0530

    Square of sorted array

---
+++ Week1/Day3/Square_of_sorted_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i<n ; i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+    
+};
--- Week1/day1/Best_time_to_buy_sell_stock.cpp
+++ Week1/day1/Best_time_to_buy_sell_stock.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& nums) {
