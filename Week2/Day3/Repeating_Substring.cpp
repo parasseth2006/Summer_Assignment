c72e72a91326d5efc95d630c51117d36e49d533b
parasseth2006 <parasseth2006@gmail.com>
2026-06-10 20:39:08 +0530

    Day3 work done

---
+++ Week2/Day3/Repeating_Substring.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string doubled = s + s;
+        return doubled.substr(1, doubled.size() - 2).find(s) != string::npos;
+    }
+};
