6aa7ac54a5ef40a6fc883c7f399ee4dd9d61e82a
parasseth2006 <parasseth2006@gmail.com>
2026-06-15 15:56:43 +0530

    Day1 work added

---
+++ Week3/Day1/Reverse_LinkedList.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
